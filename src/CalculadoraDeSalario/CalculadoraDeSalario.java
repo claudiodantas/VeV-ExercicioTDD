98f221601f67456c211ae327119ac90ce1f70f1e
Henrique-Rmc <paulo.cruz@ccc.ufcg.edu.br>
2023-04-12 09:19:13 -0300

    Estruturas criadas a partir dos requisitos test

--- src/CalculadoraDeSalario/CalculadoraDeSalario.java
+++ src/CalculadoraDeSalario/CalculadoraDeSalario.java
@@ -9,4 +9,15 @@ public class CalculadoraDeSalario {
     public int test(){
         return 2;
     }
+
+    public Double calculaSalario(Funcionario func) {
+        if (func.getCargo() == Cargo.DESENVOLVEDOR) {
+            if (func.getSalario() > 3000.0) {
+                return func.getSalario() * 0.8;
+            } else {
+                return func.getSalario() * 0.9;
+            }
+        }
+
+    }
 }
