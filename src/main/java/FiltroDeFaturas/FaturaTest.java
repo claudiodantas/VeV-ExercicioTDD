9fdb82bd6d007949c0a0172134c48aa1b80d3c49
claudiodantas <franciclaudio.silva@ccc.ufcg.edu.br>
2023-04-11 22:45:11 -0300

    add teste para a criação de uma Fatura

---
+++ src/main/java/FiltroDeFaturas/FaturaTest.java
@@ -0,0 +1,25 @@
+package FiltroDeFaturas;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FaturaTest {
+
+    @Test
+    void deveriaCriarUmaFatura() {
+        int codigo = 1;
+        double valor = 100.50;
+        Date data = new Date();
+        Cliente cliente = Mockito.mock(Cliente.class);
+
+        Fatura novaFatura = new Fatura(codigo, valor, data, cliente);
+
+        assertEquals(codigo, novaFatura.getCodigo());
+        assertEquals(valor, novaftura.getValor());
+        assertEquals(data, novaFatura.getData());
+        assertEquals(cliente, novaFatura.getCliente());
+    }
+}
