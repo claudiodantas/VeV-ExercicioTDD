5685332969d6c11e098a5b694b55e16f44f07dfc
claudiodantas <franciclaudio.silva@ccc.ufcg.edu.br>
2023-04-11 21:46:00 -0300

    add teste da criação de um objeto Cliente

---
+++ src/FiltroDeFaturas/ClienteTest.java
@@ -0,0 +1,22 @@
+package FiltroDeFaturas;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class ClienteTest {
+
+    @Test
+    void deveCriarUmCliente(){
+        String nome = "José";
+        Date dataDeInclusao = new Date();
+        String estado = "PB";
+
+        Cliente novoCliente = new Cliente(nome, dataDeInclusao, estado);
+
+        assertEquals(nome, novoCliente.getNome());
+        assertEquals(dataDeInclusao, novoCliente.getDataDeInclusao());
+        assertEquals(estado, novoCliente.getEstado());
+    }
+}
