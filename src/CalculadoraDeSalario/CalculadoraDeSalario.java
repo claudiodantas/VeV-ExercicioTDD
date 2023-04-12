8eb11a671d5f52d1ec60bdb6d0660cdc172a91ff
Henrique-Rmc <paulo.cruz@ccc.ufcg.edu.br>
2023-04-12 08:14:27 -0300

    criação do primeiro teste

--- src/CalculadoraDeSalario/CalculadoraDeSalario.java
+++ src/CalculadoraDeSalario/CalculadoraDeSalario.java
@@ -1,6 +1,6 @@
-package ToBeChanged;
+package CalculadoraDeSalario;
 
-public class ToBeChanged {
+public class CalculadoraDeSalario {
 
     public static void main(String[] args) {
         System.out.println("Hello world2!");
---
+++ src/CalculadoraDeSalario/CalculadoraDeSalarioTest.java
@@ -0,0 +1,17 @@
+package CalculadoraDeSalario;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraDeSalarioTest {
+
+    @Test
+    void test1() {
+        Funcionario func = new Funcionario("Joao", "joao@mail.com", "1000.00", Cargo.DESENVOLVEDOR);
+        CalculadoraDeSalario calc = new CalculadoraDeSalario();
+        assertEquals(900.00, calc.calculaSalario(func));
+    }
+
+
+}
